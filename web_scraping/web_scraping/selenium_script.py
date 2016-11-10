76cc598ca45a3447a3c679b59644e8d5d47fd688
Edima Udo <edimaudo@gmail.com>
2016-11-10 09:15:29 -0500

    web scraping

--- web_scraping/web_scraping/selenium_script.py
+++ web_scraping/web_scraping/selenium_script.py
@@ -1,6 +1,17 @@
-import time
 from selenium import webdriver
- 
-driver = webdriver.Firefox()
-time.sleep(5)
+
+path_to_chromedriver = '/Users/Edima/Desktop/chromedriver' #change as needed
+
+driver = webdriver.Chrome(path_to_chromedriver)
+driver.get("http://www.youtube.com/results?search_query=" + "guitar+lessons")
+
+results = driver.find_elements_by_xpath('//div[@class="yt-lockup-content"]')
+
+print(len(results))
+
+for result in results:
+    video = result.find_element_by_xpath('.//h3/a')
+    title = video.get_attribute('title')
+    url = video.get_attribute('href')
+    print("{} ({})".format(title, url))
 driver.quit()
