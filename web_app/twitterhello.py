802ea3bfb17f658142edfee1a6967518e3b04a64
Edima Udo <edimaudo@gmail.com>
2017-03-20 13:19:27 -0400

    update

---
+++ web_app/twitterhello.py
@@ -0,0 +1,17 @@
+import tweepy
+
+consumer_key = 'your_consumer_key'
+consumer_secret = 'your_consumer_secret'
+access_token = 'your_access_token'
+access_token_secret = 'your_access_token_secret'
+
+
+
+# Set up OAuth and integrate with API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Write a tweet to push to our Twitter account
+tweet = 'hello'
+api.update_status(status=tweet)
