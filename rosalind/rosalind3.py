6385f9164fc3947db08916ac3337a78fab056b10
Edima Udo <edimaudo@gmail.com>
2018-01-24 22:05:23 -0500

    rosalind

---
+++ rosalind/rosalind3.py
@@ -0,0 +1,8 @@
+stringinfo = "Hyb0rOfyEucratoscelus6BfNRBoSwgXiGK6xjwq3sR2oFIIGgwJmk0AhhkMqB9OdhJnjyyvtnyLxJFaB2mxfiUSRfjyM2PllOHqZ0Hdp04TBsIYRK6zhschrysaetoss12JWPPmN71khiimz2Ta1zM8aKZVW0wqjWDZhW9JNdFE."
+
+begin1 = 8 
+end1 = 20 
+begin2 = 118 
+end2 = 127
+outputstring = stringinfo[begin1:end1 + 1] + " " + stringinfo[begin2:end2+1]
+print(outputstring)
