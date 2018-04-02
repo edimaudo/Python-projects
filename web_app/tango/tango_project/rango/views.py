0dda99013f82b8907c654592fd5941596a39a402
Edima Udo <edimaudo@gmail.com>
2018-04-02 16:11:24 -0400

    rango app
    updated the rango app by adding a view for about and creating urls that
    can toggle between about and main.

--- web_app/tango/tango_project/rango/views.py
+++ web_app/tango/tango_project/rango/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hi!")
+	return HttpResponse("Rango says hi! <br/> <a href='/rango/about/'>About</a>")
+
+def about(request):
+	return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>index</a>")
