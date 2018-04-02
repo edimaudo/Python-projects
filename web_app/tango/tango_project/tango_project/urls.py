7dd02732388b4c0a49497d947f336de70f75d351
Edima Udo <edimaudo@gmail.com>
2018-04-02 15:38:32 -0400

    tango project url
    updated the tango project url to handle rango portion url

--- web_app/tango/tango_project/tango_project/urls.py
+++ web_app/tango/tango_project/tango_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',views.index, name='index'),
+    url(r'^rango/',include('rango.urls')),
 ]
