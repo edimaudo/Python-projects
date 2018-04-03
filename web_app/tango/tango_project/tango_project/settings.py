0ed0dc3515ef33681b1fab5110850e49c14237a7
Edima Udo <edimaudo@gmail.com>
2018-04-03 12:49:47 -0400

    settings.py
    added static path and directory

--- web_app/tango/tango_project/tango_project/settings.py
+++ web_app/tango/tango_project/tango_project/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -116,6 +117,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [STATIC_DIR, ]
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
