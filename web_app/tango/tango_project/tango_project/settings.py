17c3826b89c2bb8608406ca3dee2af5ccc82103a
Edima Udo <edimaudo@gmail.com>
2018-04-03 12:34:54 -0400

    setting.py
    updated the settings.py file using more flexible absolute paths

--- web_app/tango/tango_project/tango_project/settings.py
+++ web_app/tango/tango_project/tango_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tango_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/edima/Documents/Coding/python_projects/web_app/tango/tango_project/templates/tango_project/templates'],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
