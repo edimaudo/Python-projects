ab059f94afc2f2c51037b1cb59f29b08c0a9f92a
Edima Udo <edimaudo@gmail.com>
2018-04-03 21:03:10 -0400

    about
    updated about template

--- web_app/tango/tango_project/rango/views.py
+++ web_app/tango/tango_project/rango/views.py
@@ -12,4 +12,6 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>index</a>")
+	context_dict= {'yourname':"Edima"}
+	return render(request, 'rango/about.html', context_dict)
+	#return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>index</a>")
