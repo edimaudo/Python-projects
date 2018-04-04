7b716ca4b86e40d7df94992d369ce001491f5aa5
Edima Udo <edimaudo@gmail.com>
2018-04-04 17:36:32 -0400

    model

--- web_app/tango/tango_project/rango/models.py
+++ web_app/tango/tango_project/rango/models.py
@@ -3,3 +3,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=128, unique=True)
+	
+	class Meta:
+		verbose_name_plural = 'Categories'
+	
+	def __str__(self): 
+		return self.name
+	# For Python 2, use __unicode__ too 
+	def __unicode__(self):
+		return self.name
+	
+
+
+class Page(models.Model):
+	category = models.ForeignKey(Category) 
+	title = models.CharField(max_length=128) 
+	url = models.URLField()
+	views = models.IntegerField(default=0)
+	def __str__(self): 
+		return self.title
+	# For Python 2, use __unicode__ too 
+	def __unicode__(self):
+		return self.name
