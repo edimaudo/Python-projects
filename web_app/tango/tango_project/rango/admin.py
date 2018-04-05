7feaea6224923f3716f600f166cc8114137dbf4a
Edima Udo <edimaudo@gmail.com>
2018-04-05 13:19:50 -0400

    admin

--- web_app/tango/tango_project/rango/admin.py
+++ web_app/tango/tango_project/rango/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from rango.models import Category, Page
+#from rango.models import UserProfile
 # Register your models here.
+
+#admin.site.register(Page, PageAdmin)
+
+# Create your models here.
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category)
+#admin.site.register(Page, PageAdmin)
+admin.site.register(Page)
