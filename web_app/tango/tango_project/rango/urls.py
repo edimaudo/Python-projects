92fb06d10ce83fcfd3dd24188a91bf22e7d197a6
Edima Udo <edimaudo@gmail.com>
2018-04-07 07:15:30 -0400

    update

--- web_app/tango/tango_project/rango/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from rango import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-url(r'^$',views.index,name='index'),
-url(r'^about/',views.about,name='about'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
