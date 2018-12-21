bf51302d0b60145349662f6cfe4f284ca971cffa
Edima Udo <edimaudo@gmail.com>
2018-12-20 21:11:49 -0500

    Update problem_3.py

--- daily_coding/problem_3.py
+++ daily_coding/problem_3.py
@@ -1,13 +1,26 @@
-Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+# Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
 
-For example, given the following Node class
+# For example, given the following Node class
 
+# class Node:
+#     def __init__(self, val, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# The following test should pass:
+
+# node = Node('root', Node('left', Node('left.left')), Node('right'))
+# assert deserialize(serialize(node)).left.left.val == 'left.left'
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-The following test should pass:
 
-node = Node('root', Node('left', Node('left.left')), Node('right'))
-assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+def main():
+	node = Node('root', Node('left', Node('left.left')), Node('right'))
+	assert deserialize(serialize(node)).left.left.val == 'left.left'
+
+if "__name__" == __main__:
+	main()
