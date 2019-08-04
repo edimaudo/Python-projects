7c02a772eee509524531dd2c693f3ac534aec182
Edima Udo <edimaudo@gmail.com>
2019-08-03 22:38:09 -0400

    Create optimization.py

---
+++ programming_collective_intelligence/optimization.py
@@ -0,0 +1,12 @@
+import time
+import random
+import math
+
+people = [('Seymour','BOS'),
+('Franny','DAL'),
+('Zooey','CAK'),
+('Walt','MIA'),
+('Buddy','ORD'),
+('Les','OMA')]
+# LaGuardia airport in New York
+destination='LGA'
