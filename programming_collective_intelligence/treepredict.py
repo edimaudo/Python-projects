1dce56a2785ae6efba6194d821ac0711f86b26e9
Edima Udo <edimaudo@gmail.com>
2019-08-06 20:47:36 -0400

    Update treepredict.py

--- programming_collective_intelligence/treepredict.py
+++ programming_collective_intelligence/treepredict.py
@@ -36,6 +36,34 @@ if r not in results: results[r]=0
 results[r]+=1
 return results
 
+
+# Probability that a randomly placed item will
+# be in the wrong category
+def giniimpurity(rows):
+total=len(rows)
+counts=uniquecounts(rows)
+imp=0
+for k1 in counts:
+p1=float(counts[k1])/total
+for k2 in counts:
+if k1==k2: continue
+p2=float(counts[k2])/total
+imp+=p1*p2
+return imp
+
+# Entropy is the sum of p(x)log(p(x)) across all
+# the different possible results
+def entropy(rows):
+from math import log
+log2=lambda x:log(x)/log(2)
+results=uniquecounts(rows)
+# Now calculate the entropy
+ent=0.0
+for r in results.keys( ):
+p=float(results[r])/len(rows)
+ent=ent-p*log2(p)
+return ent
+
 # Divides a set on a specific column. Can handle numeric
 # or nominal values
 def divideset(rows,column,value):
