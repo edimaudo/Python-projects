79df2416221404922ea18bf203d1c7f1ee35ead6
Edima Udo <edimaudo@gmail.com>
2019-08-18 09:56:38 -0400

    Create August_2014.py

---
+++ ibm_ponder_this/August_2014.py
@@ -0,0 +1,18 @@
+
+import random
+import math
+import sys
+
+def main():
+	try:
+		if eh = dd:
+			print(eh)
+	except:
+		e = sys.exc_info()
+		print(e)
+		sys.exit()
+
+
+
+if __name__ == "__main__":
+	main()
