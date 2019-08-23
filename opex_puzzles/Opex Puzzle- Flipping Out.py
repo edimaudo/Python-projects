9afe08e0c2edcc2d3183c7843bf4ad7be8f72da1
Edima Udo <edimaudo@gmail.com>
2019-08-22 21:38:16 -0400

    Update Opex Puzzle- Flipping Out.py

--- opex_puzzles/Opex Puzzle- Flipping Out.py
+++ opex_puzzles/Opex Puzzle- Flipping Out.py
@@ -7,8 +7,22 @@ def generate_random(alist):
 
 def main():
 	try:
-		startInfo = [0,1]
-		print(generate_random(startInfo))
+		player1 = []
+		player2 = []
+		flipInfo = [0,1]
+		for value in range(0,100000):
+			endGame = True
+			while (endGame):
+				if generate_random(flipInfo) == 0:
+					player1.append(1)
+					player2.append(0)
+					endGame = False
+				if generate_random(flipInfo) == 0:
+					player2.append(1)
+					player1.append(0)
+					endGame = False
+		print("player 1 win rate is: " + str(sum(player1)/100000.0))
+		print("player 2 win rate is: " + str(sum(player2)/100000.0))
 	except:
 		e = sys.exc_info()
 		print(e)
