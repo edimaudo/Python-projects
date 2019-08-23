7d9c7c679561dda1a4b3964b943bd985c8442a85
Edima Udo <edimaudo@gmail.com>
2019-08-22 21:03:49 -0400

    Update Opex Puzzle- Flipping Out.py

--- opex_puzzles/Opex Puzzle- Flipping Out.py
+++ opex_puzzles/Opex Puzzle- Flipping Out.py
@@ -2,12 +2,16 @@ import math
 import random
 import sys
 
+def generate_random(alist):
+    return random.randint(0,len(alist)-1)
+
 def main():
 	try:
 		startInfo = [0,1]
+		print(generate_random(startInfo))
 	except:
 		e = sys.exc_info()
 		print(e)
 
 if __name__ == "__main__":
-	main
+	main()
