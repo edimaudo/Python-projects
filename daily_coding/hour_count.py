1a8c5260d6085c5a419c788ee5ffb6f5aa5ae57c
Edima <edimaudo@gmail.com>
2020-09-12 19:20:11 -0400

    Update hour_count.py

--- daily_coding/hour_count.py
+++ daily_coding/hour_count.py
@@ -10,13 +10,25 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('hrdata.csv')
+df = pd.read_csv('FinalData.csv')
+df.sort_values(by=['IpAddress','date_time'], inplace=False) #sort df
 
-order_information = []
+df_temp = df.head(5)
+print(df_temp.iloc[1]['date_time'])
 
-index = 0
-for i, row in df.iterrows():
+data_info = []
+
+value = 0
+for i, row in df_temp.iterrows():
 	if i == 0:
-		#do something
+		value = value + 1
+		data_info.append([row['IpAddress'],row['date_time'],row['data'],value])
 	else:
-		
+		t1 = pd.to_datetime(df_temp.iloc[i-1]['date_time'])
+		t2 = pd.to_datetime(df_temp.iloc[i]['date_time'])
+		if pd.Timedelta(t2 - t1).seconds >= 20:
+			value = value + 1
+			data_info.append([row['IpAddress'],row['date_time'],row['data'],value])
+
+df_final = pd.DataFrame(data_info)
+df_final.columns = ['IpAddress','date_time','data','value']
