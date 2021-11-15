27e9a3561f2324baeb299f4cc415eb6fbca6f4f1
Edima <edimaudo@gmail.com>
2021-11-14 19:30:16 -0500

    Update app.py

---
+++ data_analytics/smart_rehab/app.py
@@ -0,0 +1,16 @@
+#================
+# Smart Rehabililtation
+# A client of yours is Maha, a Physiotherapist who helps people build customized rehabilitation plans that fits their
+# health conditions and abilities. Maha told you that it takes her a long time to build rehabilitation exercise
+# plans for her customers since it varies according to the customer Age Category, Condition and No. of Exercises.
+# So, she decided to reach out to your company to see if AI can make her job easier. You told her that she is
+# lucky since this is a common AI problem, known as optimization, and you can help her by building a Genetic algorithm solution
+#================
+
+# Load libraries
+import streamlit as st
+import pandas as pd
+
+st.title('Smart Rehabililtation')
+
+
