58e4e118ea4c467c39c9a6af10fd0090307111d5
Edima <edimaudo@gmail.com>
2021-11-14 22:18:40 -0500

    Update app.py

--- data_analytics/smart_rehab/app.py
+++ data_analytics/smart_rehab/app.py
@@ -11,7 +11,7 @@
 import streamlit as st
 import pandas as pd
 import pygad
-import numpy as np
+import numpy
 
 st.title('Smart Rehabililtation')
 
@@ -38,13 +38,34 @@ else:
 
 if condition_choice == "Brain Injury":
 	condition_option_choice = 1
-else if condition_choice == "Spinal Cord":
+elif condition_choice == "Spinal Cord":
 	condition_option_choice = 2
 else:
 	condition_option_choice = 3
 
-function_input
-desired
+function_inputs = [age_option_choice, condition_option_choice, elbow_choice, upper_arm_choice, knee_leg_choice, wrist_choice]
+desired_output = 44
+
+def fitness_func(solution, solution_idx):
+	output = numpy.sum(solution*function_inputs)
+	fitness = 1.0 / (numpy.abs(output - desired_output) + 0.000001)
+	return fitness
+
+ga_instance = pygad.GA(num_generations=100, #20000
+                       sol_per_pop=100,
+                       num_genes=len(function_inputs),
+                       num_parents_mating=2,
+                       fitness_func=fitness_func,
+                       mutation_type="random")
+
+if submit_button:
+	data_load_state = st.text('Running GA...')
+	ga_instance.run()
+	data_load_state.text('GA done!')
+	st.pyplot(ga_instance.plot_result())
+	st.write(best_solution())
+
+
 
 # Best rehabilitation
 # exercise(s) that maximize your fitness function and output a complete plan for each body parts (elbow,
@@ -60,4 +81,5 @@ desired
 #=================
 # GA Output
 #=================
-st.write("Your rehabilitation plan is ready! Your plan is presented below with these exercises per day. ")
+if submit_button:
+	st.write("Your rehabilitation plan is ready! Your plan is presented below with these exercises per day. ")
