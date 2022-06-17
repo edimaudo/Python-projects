85bf23cf8185b0b5f7af2e7263d5fd7fc7b834db
Edima <edimaudo@gmail.com>
2022-06-16 20:17:18 -0400

    Update Overview.py

--- streamlit/book_recommender/Overview.py
+++ streamlit/book_recommender/Overview.py
@@ -11,10 +11,33 @@ import os, os.path
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title('Book Recommender')
+st.title('Book Overview')
 
+@st.cache
+def load_data(DATA_URL):
+    data = pd.read_csv(DATA_URL)
+    data.dropna(inplace=True)
+    return data
 
-st.header("Summary")
+# Load data
+books = load_data("BX-Books_clean.csv")
+users = load_data("BX-Users_clean.csv") 
+ratings = load_data("BX-Book-Ratings_clean.csv") 
+
+#====================
+# Raw data
+#====================
+st.subheader("Book data")
+with st.expander("Open to see more",expanded=False):
+    st.dataframe(books)
+
+st.header("Book Overview")
+name = st.text_input("Book Search")
+
+search_df = books.apply(lambda row: row.astype(str).str.contains('name').any(), axis=1)
+#st.metric("User Count", 42)
+#st.metric("Average Rating", 42)
+
 
 
 
