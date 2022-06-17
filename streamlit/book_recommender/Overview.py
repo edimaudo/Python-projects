e849df853b7386a35acd326d2e4ce96431654892
Edima <edimaudo@gmail.com>
2022-06-17 19:16:46 -0400

    Update Overview.py

--- streamlit/book_recommender/Overview.py
+++ streamlit/book_recommender/Overview.py
@@ -1,5 +1,5 @@
 #================
-# Book Recommender
+# Book Overview
 #================
 
 
@@ -8,7 +8,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import os, os.path
-import pandas as pd
 import matplotlib.pyplot as plt
 
 st.title('Book Overview')
@@ -24,6 +23,8 @@ books = load_data("BX-Books_clean.csv")
 users = load_data("BX-Users_clean.csv") 
 ratings = load_data("BX-Book-Ratings_clean.csv") 
 
+
+
 #====================
 # Raw data
 #====================
@@ -31,12 +32,12 @@ st.subheader("Book data")
 with st.expander("Open to see more",expanded=False):
     st.dataframe(books)
 
-st.header("Book Overview")
-name = st.text_input("Book Search")
+c = st.container()
 
-search_df = books.apply(lambda row: row.astype(str).str.contains('name').any(), axis=1)
-#st.metric("User Count", 42)
-#st.metric("Average Rating", 42)
+
+# Data aggregation
+# books + ratings
+
 
 
 
