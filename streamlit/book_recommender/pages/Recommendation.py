75a9a95bfcbf6d4198a17ee76bc82fb75967401f
Edima <edimaudo@gmail.com>
2022-06-17 19:17:02 -0400

    Update Recommendation.py

--- streamlit/book_recommender/pages/Recommendation.py
+++ streamlit/book_recommender/pages/Recommendation.py
@@ -6,4 +6,21 @@ import os, os.path
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title('Book Overview')
+#scikit-surprise
+st.title('Book Recommender')
+
+from surprise import Reader, Dataset, SVD
+from surprise.model_selection.validation import cross_validate
+reader = Reader()
+data = Dataset.load_from_df(df[['Cust_Id', 'Movie_Id', 'Rating']], reader)
+svd = SVD()
+
+# Run 5-fold cross-validation and print results
+cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+
+trainset = data.build_full_trainset()
+svd.fit(trainset)
+
+titles = titles.sort_values(by=['Estimate_Score'], ascending=False)
+titles.head(10)
