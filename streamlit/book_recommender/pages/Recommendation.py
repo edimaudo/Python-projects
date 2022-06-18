00e6b8990dc821b7c1b95a21acab213bd754a97b
Edima <edimaudo@gmail.com>
2022-06-18 07:39:28 -0400

    Update Recommendation.py

--- streamlit/book_recommender/pages/Recommendation.py
+++ streamlit/book_recommender/pages/Recommendation.py
@@ -9,18 +9,21 @@ import matplotlib.pyplot as plt
 #scikit-surprise
 st.title('Book Recommender')
 
-from surprise import Reader, Dataset, SVD
-from surprise.model_selection.validation import cross_validate
-reader = Reader()
-data = Dataset.load_from_df(df[['Cust_Id', 'Movie_Id', 'Rating']], reader)
-svd = SVD()
+# from surprise import Reader, Dataset, SVD
+# from surprise.model_selection.validation import cross_validate
+# reader = Reader()
+# data = Dataset.load_from_df(df[['Cust_Id', 'Movie_Id', 'Rating']], reader)
+# svd = SVD()
 
-# Run 5-fold cross-validation and print results
-cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+# # Run 5-fold cross-validation and print results
+# cross_validate(svd, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
 
 
-trainset = data.build_full_trainset()
-svd.fit(trainset)
+# trainset = data.build_full_trainset()
+# svd.fit(trainset)
 
-titles = titles.sort_values(by=['Estimate_Score'], ascending=False)
-titles.head(10)
+# titles = titles.sort_values(by=['Estimate_Score'], ascending=False)
+# titles.head(10)
+
+# # build recommender tool
+# # take one or two inputs
