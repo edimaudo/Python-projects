de741d211f23b55a728e1e8775b73c052e0e0a74
Edima <edimaudo@gmail.com>
2022-06-17 20:54:20 -0400

    Update Overview.py

--- streamlit/book_recommender/Overview.py
+++ streamlit/book_recommender/Overview.py
@@ -1,8 +1,6 @@
 #================
 # Book Overview
 #================
-
-
 # Load libraries
 import streamlit as st
 import pandas as pd
@@ -23,8 +21,6 @@ books = load_data("BX-Books_clean.csv")
 users = load_data("BX-Users_clean.csv") 
 ratings = load_data("BX-Book-Ratings_clean.csv") 
 
-
-
 #====================
 # Raw data
 #====================
@@ -32,11 +28,46 @@ st.subheader("Book data")
 with st.expander("Open to see more",expanded=False):
     st.dataframe(books)
 
-c = st.container()
-
-
 # Data aggregation
 # books + ratings
+
+st.subheader("Book Data Summary")
+
+# Metrics
+metric_container = st.container()
+top_bottom_5_container = st.container()
+book_year_container = st.container()
+
+# AVERAGE RATING
+average_rating = "{:.2f}".format(ratings['bookRating'].mean())
+
+# NUMBER OF USERS
+number_of_users = users['userID'].count()
+
+# NUMBER OF BOOKS
+number_of_books = books['ISBN'].count()
+
+# TOP 5 PUBLISHERS AND BOTTOM 5 PUBLISHERS
+top_5_publishers = ""
+bottom_5_publishers = ""
+
+# TOP 5 BOOKS AND BOTTOM 5 BOOKS
+top_5_publishers = ""
+bottom_5_publishers = ""
+
+# TOP 5 AUTHORS and bottom 5 AUTHORS
+top_5_publishers = ""
+bottom_5_publishers = ""
+# TOP AND BOTTOM 5 COUNTRIES
+top_5_publishers = ""
+bottom_5_publishers = ""
+# NUMBER OF BOOKS BY YEAR COUNT GRAPH
+number_books_year = ""
+
+
+
+
+
 
 
 
