2c06f2adebe8e3a1c973775c86ad714a21433560
Edima <edimaudo@gmail.com>
2022-06-18 18:38:54 -0400

    Create Book Search.py

---
+++ streamlit/book_recommender/pages/Book Search.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache
+def load_data(DATA_URL):
+    data = pd.read_csv(DATA_URL)
+    data.dropna(inplace=True)
+    return data
+
+# Load data
+books = load_data("BX-Books_clean.csv")
+users = load_data("BX-Users_clean.csv") 
+ratings = load_data("BX-Book-Ratings_clean.csv") 
+
+st.header("Book Overview")
+name = st.text_input("Search by Author or Book Title")
+search_df = books.apply(lambda row: row.astype(str).str.contains('name').any(), axis=1)
+
+if search_df.empty:
+	st.metric("User Count", 42)
+	st.metric("Average Rating", 42)
+else:
+	# get ratings data
+	st.metric("User Count", 42)
+	st.metric("Average Rating", 42)	
+
+
+# Search OUtput
+# average ratings
+# publisher
+# ISBN
+# of ratings
+# user count
+# top 5 countries 
+# Year of publication
+# ISBN
