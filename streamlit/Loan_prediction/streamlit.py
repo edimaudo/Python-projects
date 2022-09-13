3e8765f27ddc22055f5b4eeb95c663902d836b50
Edima <edimaudo@gmail.com>
2022-09-13 19:58:00 -0400

    Create streamlit.py

---
+++ streamlit/Loan_prediction/streamlit.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import base64
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pickle as pkl
+import shap
+import streamlit.components.v1 as components
+import os, os.path
+
+
+
+######################
+# Load data
+######################
+
+######################
+# Load model
+######################
+
+######################
+# Main Page
+######################
+
+######################
+# Sidebar 
+######################
+
+btn_predict = st.sidebar.button("Predict")
+
+######################
+# Model
+######################
