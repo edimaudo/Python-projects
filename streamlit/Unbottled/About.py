ee71420c665524dd4ab9dfbf558014a0785f1b52
Edima <edimaudo@gmail.com>
2022-10-06 18:38:25 -0400

    Create About.py

---
+++ streamlit/Unbottled/About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('OTF Charity Insights')
+st.write(""" 
+Unbottled is a wine exploration and recommendation app.  provides insights into prices, ratings, descriptions, and geographic distribution of the world's most esteemed wines. Novice or connoisseur, consumer or seller, this app will meet your oenophile needs.
+
+""")
