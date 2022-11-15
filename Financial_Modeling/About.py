52aeaad39e6858f1336eb20a1e24c2f226509fcc
Edima <edimaudo@gmail.com>
2022-11-15 14:13:31 -0500

    Update About.py

--- Financial_Modeling/About.py
+++ Financial_Modeling/About.py
@@ -11,6 +11,14 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import math 
 import re, string
 
+
+st.set_page_config(
+    page_title = "Financial Modeling Using Python",
+    layout = 'wide'
+)
+
+st.title('Financial Modeling using Python')
+
 st.header("About")
 st.write("""
 This application leverages Nick DeRobertis’ Financial Modeling [course](https://nickderobertis.github.io/fin-model-course/index.html).  The application will have different pages that focus on solving the projects associated with the [course](https://nickderobertis.github.io/fin-model-course/index.html).  The application will be built using streamlit.
