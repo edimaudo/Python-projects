da86cd8e9a5e024a87457e1f514cb5f14ac31dc4
Edima <edimaudo@gmail.com>
2022-12-11 12:51:34 -0500

    Update app.py

--- streamlit/bcliquor/app.py
+++ streamlit/bcliquor/app.py
@@ -2,14 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os, os.path
-
+import plotly.express as px
 
 ######################
 # Load data
 ######################
 path = os.path.dirname(__file__)
-DATA_URL = path + "/bcl-data.csv"
-
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    return data
+DATA_URL = path + "bcl-data.csv"
+df = load_data()
 
 ######################
 # Title
@@ -21,22 +24,32 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 ######################
 # Main Page
 ######################
-st.title("BC Liquor Store Prices)
+st.title("BC Liquor Store Prices")
 
 ######################
 # Sidebar 
 ######################
-st.sidebar.title("Loan Applicant Information")
-st.sidebar.image("ab.png", width=100)
-st.sidebar.write("Please choose parameters that descibe the applicant")
-
 # Input features
-loan_amnt =st.sidebar.slider("Select the loan amount: ",min_value=1000, max_value=40000,step=500)
-term = st.sidebar.radio("Select Loan term: ", ('36 months', '60 months'))
-int_rate = st.sidebar.slider("Select the interest rate: ",min_value=0.05, max_value=1.00,step=0.05)
-installment = st.sidebar.slider("Select the installment amount: ",min_value=10, max_value=400,step=50)
+price_input = st.sidebar.slider("Price ($): ",min_value=0.0, max_value=100.0,step=5.0, value=(25.0,40.0))
+product_input = st.sidebar.radio("Product ", ("BEER", "REFRESHMENT", "SPIRITS", "WINE"), index=3)
+
+country_list = df['Country'].unique()
+country_list  = country_list.astype('str')
+country_list.sort()
+
+country_input = st.sidebar.selectbox("Country",country_list)
 
 ######################
 # Output
 ######################
+store_analysis = df[(df.Price.isin(price_input)) |
+                (df.Type == product_input) | 
+                (df.Country == country_input)]
+
+df_alchol_agg = df.groupby(['Alcohol_Content']).size().reset_index(name='count')
+df_alchol_agg.columns = ['Alcohol Content', 'Count']
+fig = px.bar(df_alchol_agg, x="Alcohol Content", y="Count")
+st.plotly_chart(fig)
+
+st.dataframe(store_analysis)
 
