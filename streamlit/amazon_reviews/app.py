040ee5158d01b0222a82b449c5356fef11d01aa8
Edima <edimaudo@gmail.com>
2022-12-13 07:52:15 -0500

    Update app.py

--- streamlit/amazon_reviews/app.py
+++ streamlit/amazon_reviews/app.py
@@ -1,3 +1,6 @@
+######################
+# Load Libraries
+######################
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -5,28 +8,30 @@ import os, os.path
 import datetime
 import re, string
 
-
-st.set_page_config( 
-    page_title="Amazon reviews for Vitamin C Products",
-)
-
+######################
 # Load data
+######################
 @st.cache(allow_output_mutation=True)
 def load_data():
     #used to fix utf-8 error (https://stackoverflow.com/questions/46000191/utf-8-codec-cant-decode-byte-0x92-in-position-18-invalid-start-byte)
     data = pd.read_csv(DATA_URL,encoding='cp1252') 
     return data
-
-DATA_URL = "data.csv"
+DATA_URL = path + "data.csv"
 df = load_data()
 
+######################
+# Title
+######################
+st.set_page_config(
+    page_title="Amazon reviews for Vitamin C Products",
+)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-
-# Overview
 
 
 # Summary
 
+# Overview
 
 # Visualization
 
